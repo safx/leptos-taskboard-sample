aba9beea9c9d3656f7210efa629f4a761af119d8
Yuji MATSUMOTO <matsumoto@nulab.com>
2022-11-19 13:24:14 +0900

    add Column component

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,24 @@ pub fn Board(cx: Scope) -> Element {
             <section class="section">
                 <div class="container">
                     <div class="columns">
-                        <div class="column">
-                            <div class="tags has-addons">
-                                <span class="tag">"Open"</span>
-                                <span class="tag is-dark">0</span>
-                            </div>
-                        </div>
+                        <Column text="Open" />
+                        <Column text="In progress" />
+                        <Column text="Completed" />
                     </div>
                 </div>
              </section>
+        </div>
+    }
+}
+
+#[component]
+fn Column(cx: Scope, text: &'static str) -> Element {
+    view ! { cx,
+        <div class="column">
+            <div class="tags has-addons">
+                <span class="tag">{ text }</span>
+                <span class="tag is-dark">{ 0 }</span>
+            </div>
         </div>
     }
 }
