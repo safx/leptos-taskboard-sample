3661cdec76686cb89510bc4c4cd3f2f197f848f2
Yuji MATSUMOTO <matsumoto@nulab.com>
2023-02-27 22:04:58 +0900

    add handlers for styles and a script

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::net::SocketAddr;
 use axum::{routing::get, Router, response::Html};
+use axum::error_handling::HandleError;
+use axum::handler::HandlerWithoutStateExt;
+use tower_http::services::{ServeFile, ServeDir};
+use http::StatusCode;
 
 #[tokio::main]
 async fn main() {
@@ -11,13 +15,25 @@ async fn main() {
             <html lang="en">
             <head>
                 <meta charset="utf-8"/>
+                <link rel="stylesheet" href="/style.css">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
             </head>
+            <script type="module">import init, { main } from './pkg/taskboard.js'; init().then(main);</script>
             <body>
             </body>
             </html>"#)
     }
 
+    let pkg_service = ServeDir::new("pkg").not_found_service(handle_file_error.into_service());
+    let style_service = ServeFile::new("style.css");
+
+    async fn handle_file_error() -> (StatusCode, &'static str) {
+        (StatusCode::NOT_FOUND, "File Not Found")
+    }
+
     let app = Router::new()
+                  .nest_service("/pkg", pkg_service)
+                  .nest_service("/style.css", style_service)
                   .route("/", get(root));
 
     println!("listening on http://{}", &addr);
