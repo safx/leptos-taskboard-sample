c4541c96f1a9ac16d3e9706513e0d501c6c34529
Yuji MATSUMOTO <matsumoto@nulab.com>
2023-02-27 21:35:33 +0900

    add axum and tokio

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-use leptos::*;
-use taskboard::*;
+use std::net::SocketAddr;
+use axum::{routing::get, Router, response::Html};
 
-fn main() {
-    mount_to_body(|cx| view! { cx, <Board /> })
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    async fn root() -> Html<&'static str> {
+        Html(
+         r#"<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+            </head>
+            <body>
+            </body>
+            </html>"#)
+    }
+
+    let app = Router::new()
+                  .route("/", get(root));
+
+    println!("listening on http://{}", &addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
