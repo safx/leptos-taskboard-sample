c488889b2c2dd2f94135b50f7cafa0babab367cd
Safx <safxdev@gmail.com>
2024-06-22 23:09:59 +0900

    update to leptos-0.6.12

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,26 +1,53 @@
-use leptos::{get_configuration, view};
-use axum::{routing::post, Router};
-use tower_http::services::{ServeFile, ServeDir};
+use axum::{
+    body::Body,
+    extract::State,
+    http::{Request, Uri},
+    response::{IntoResponse, Response},
+};
+use leptos::{get_configuration, LeptosOptions, view};
+use axum::Router;
 use taskboard::Board;
+use leptos_axum::{generate_route_list, LeptosRoutes};
+use tower::ServiceExt;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     let conf = get_configuration(Some("Cargo.toml")).await.unwrap();
     let leptos_options = conf.leptos_options;
-    let addr = leptos_options.site_addr.clone();
-
-    let pkg_service = ServeDir::new("./pkg");
-    let style_service = ServeFile::new("style.css");
+    let addr = leptos_options.site_addr;
+    let routes = generate_route_list(Board);
 
     let app = Router::new()
-               .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
-               .nest_service("/pkg", pkg_service)
-               .nest_service("/style.css", style_service)
-               .fallback(leptos_axum::render_app_to_stream(leptos_options, || view! { <Board /> }));
+               .leptos_routes(&leptos_options, routes, Board)
+               .fallback(file_handler)
+               .with_state(leptos_options);
 
     println!("listening on http://{}", &addr);
-    axum::Server::bind(&addr)
-       .serve(app.into_make_service())
-       .await
-       .unwrap();
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+pub async fn file_handler(
+    uri: Uri,
+    State(options): State<LeptosOptions>,
+    req: Request<Body>,
+) -> Response {
+    let file_req = Request::builder()
+        .uri(uri.clone())
+        .body(Body::empty())
+        .unwrap();
+
+    match ServeDir::new(options.site_root.clone()).oneshot(file_req).await {
+        Ok(res) => res.into_response(),
+        Err(err) => {
+            let handler =
+                leptos_axum::render_app_to_stream(options.to_owned(), move || {
+                    view! { <div>{err.to_string()}</div> }
+                });
+            handler(req).await.into_response()
+        }
+    }
 }
